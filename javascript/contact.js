02ea770c1f127189b54abc7dd54c75d128c8f423
Shane <sdarcy@redventures.com>
2017-07-27 16:59:02 -0400

    resume - got rid <hr> tags

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,52 +1,62 @@
-//Variables
-var $firstname = $('#firstname').val();
-var $lastname = $('#lastname').val();
-var $email = $('#lastname').val();
-var $phone = $('#phone').val();
-var $company = $('#company').val();
+$(document).ready(function(){
+   $('#formSubmission').submit(function(e){
+     e.preventDefault();
+       console.log(e);
+       
+       //Variables
+        var $firstname = $('#firstname').val();
+        var $lastname = $('#lastname').val();
+        var $email = $('#email').val();
+        var $phone = $('#phone').val();
+        var $company = $('#company').val();
+        var $phoneValidator = /[0-9-()+]{3,20}/;
+        var $emailValidator = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$';
+        var $namesValidator = '/^[a-z0-9_-]{3,16}$/';
+       debugger;
 
+        //All - Make sure all form inputs (not textarea) have at least 1 character before populating the "Submit" button
 
-var $phoneValidator = /[0-9-()+]{3,20}/;
-var $emailValidator = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$';
-var $namesValidator = '/^[a-z0-9_-]{3,16}$/';
+        //First Name Validator Function
+        function firstNameValidation(){
+            if ($firstname.match($namesValidator)===true){
+                alert("This works!");
+            } else {
+               alert("Please make sure to include Alpha-numeric character only."); 
+            };
+        };
+        console.log($firstname);
 
+        //Last Name Validator Function
+        function lastNameValidation(){
+            if ($lastname.match($namesValidator)===true){
+                alert("This works!");
+            } else {
+               alert("Please make sure to include Alpha-numeric character only."); 
+            };
+        };
+        console.log($lastname);
 
+        //Email - Make the email validate - contains "@" symbol and ".extension"
+        function emailValidation(){
+            if ($email.match($emailValidator)===true){
+                alert("This works!");
+            } else {
+               alert("Please make sure to include Alpha-numeric character only."); 
+            };
+        };
 
-//All - Make sure all form inputs (not textarea) have at least 1 character before populating the "Submit" button
-function nameValidation(){
-    preventDefault;
-    if ($firstname.match($namesValidator)){
-    alert("This works!");
-    } else {
-       alert("Please make sure to include Alpha-numeric character only."); 
-    };
-};
-
-
-
-//Email - Make the email validate - contains "@" symbol and ".extension"
-function emailValidation(){
-    preventDefault;
-    if ($email.match($emailValidator)){
-        alert("This works!");
-    } else {
-       alert("Please make sure to include Alpha-numeric character only."); 
-    };
-};
-
-
-
-
-//Phone - Make sure phone has at least 10 digits
-function nameValidation(){
-    preventDefault;
-    if ($phone.match($phoneValidator)){
-        alert("This works!");
-    } else {
-       alert("Please make sure to include Alpha-numeric character only."); 
-    };
-};
-
+        //Phone - Make sure phone has at least 10 digits
+        function nameValidation(){
+            if ($phone.match($phoneValidator)===true){
+                alert("This works!");
+            } else {
+               alert("Please make sure to include Alpha-numeric character only."); 
+            };
+        };
+    $('.submit').attr("disabled", true);
+    return true;   
+  }); 
+});
 
  
 
