364a434768eeb3a7a11537128ebc8a6428b65ac5
Shane <sdarcy@redventures.com>
2017-08-07 16:24:34 -0400

    contact.js getting closer to working

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,108 +1,124 @@
-$(document).ready(function(){
-   $('.submit').css("opacity", ".6");
+$(".submit").click(function(e) { 
+    e.preventDefault();
 
     //Variables
-    var $firstname, $lastname, $email, $phone, $company;
-    var $phoneValidator = /[0-9-()+]{3,20}/;
-    var $emailValidator = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$';
-    var $namesValidator = '/^[a-z0-9_-]{3,16}$/';
+    let $firstname, $lastname, $email, $phone, $company;
+    var regex = new RegExp("^[a-zA-Z]+$");
+    var phoneregex = new RegExp(/[0-9-()+]{3,20}/);
+    var emailregex = new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$');
     
-      
-//        $('input').on("keydown", function() {
-//            var inputValues = {
-//                $firstname : $('#firstname').val(),
-//                $lastname : $('#lastname').val(),
-//                $email : $('#email').val(),
-//                $phone : $('#phone').val(),
-//                $company : $('#company').val()
-//            }
-//            console.log('inputValues');
-//            
-//            for(var prop in inputValues) {
-//                console.log('inputValues');
-//                
-//                if(inputValues.prop !==""){
-//                    
-//                    //Submit button changes opacity when all 5 fields have inputs filled out
-//                    $('.submit').css('opactity', '1');
-//                    
-//                }
-//            };     
-//        });
-            
+//Input Values  
+        $firstname = $('#firstname').val();
+        $lastname = $('#lastname').val();
+        $email = $('#email').val();
+        $phone = $('#phone').val();
+        $company = $('#company').val();
 
-
-          
+    console.log($firstname);
     
-//--------------This Works!------------------------//     
-    $(".submit").click(function(e) {  
-        //.filter() creates a new array will all elements that pass the test implemented by the provided function        
-        var filledInputs = $(this).parent().find("input").filter(function(e) {
+
+//--------------All Inputs Field Checker------------------------//     
+    //.filter() creates a new array will all elements that pass the test implemented by the provided function        
+        var filledInputs = $(this).parent().find("input").filter(function(e){
             return this.value === "";
         });
-            if(filledInputs.length) {
-                alert('Please make sure that all fields are complete before clicking submit.');
-                e.preventDefault(); 
-            } else {
-                alert('Thank you for filling out the contact page.');
-            };
-        });
-//--------------This Works!------------------------//
+        if(filledInputs.length) {
+            alert('Please make sure that all fields are complete before clicking submit.');
+            e.preventDefault(); 
+        } else {
+            return $('#p2').text('Thank you for filling out the contact page.');
+        };
+   
     
-}); 
-
-    
-
-//    
-//    //All - Make sure all form inputs (not textarea) have at least 1 character before populating the "Submit" button
-
-
-//       
 //First Name Validator Function
-//        function firstNameValidation(){
-//            if ($firstname.match($namesValidator)===true){
-//                alert("This works!");
-//            } else {
-//               alert("Please make sure to include Alpha-numeric character only."); 
-//            };
-//        };
-//        console.log($firstname);
-//
+        function firstNameValidation(){
+            if(regex.test($firstname.length)){
+                return true;
+                $('#firstname').focus();
+            } else {
+               return alert(`Your description: " ${$('#firstname').val()}" is not a good description. Please insert alphabet characters only.`); 
+            }
+        };
+        firstNameValidation();
+        console.log($firstname);
+
 //Last Name Validator Function
-//        function lastNameValidation(){
-//            if ($lastname.match($namesValidator)===true){
-//                alert("This works!");
-//            } else {
-//               alert("Please make sure to include Alpha-numeric character only."); 
-//            };
-//        };
-//        console.log($lastname);
-//
+        function lastNameValidation($lastname){
+            if(regex.test($lastname)){
+                return true;
+            } else {
+               return alert(`Your description: " ${$('#lastname').val()}" is not a good description. Please insert alphabet characters only.`);
+                $('#lastname').focus();
+            };
+        };
+        lastNameValidation();
+        console.log($lastname);
+
 //Email - Make the email validate - contains "@" symbol and ".extension"
-//        function emailValidation(){
-//            if ($email.match($emailValidator)===true){
-//                alert("This works!");
-//            } else {
-//               alert("Please make sure to include Alpha-numeric character only."); 
-//            };
-//        };
+        function emailValidation($email){
+            if(emailregex.test($email)){
+                return true;
+            } else {
+               return alert(`Your description: " ${$('#email').val()}" is not a good description. Please insert alphabet characters only.`);
+                $('#email').focus();
+            };  
+        };
+        console.log($email);
+        emailValidation();
+        
+//Phone - Make sure phone is only numeric characters and has at least 10 digits
+        function phoneValidation($phone){
+            if (phoneregex.test($phone)){
+                return true;
+//            } else if ($phone.length < 10){
+//                $('#phoneAlert').innerText = "Please make sure that your phone number is 10 digits long.";
+                $('#phone').focus();
+            } else {
+               return alert(`Your description: " ${$('#phone').val()}" is not a good description. Please insert alphabet characters only.`);
+                $('#phone').focus();
+            };
+        };
+        phoneValidation();
+        console.log($phone); 
+
+//Company Validator funchtion   
+        function companyValidation($company){
+            if (regex.test($company)){
+                return true;
+            } else {
+               return alert(`Your description: " ${$('#company').val()}" is not a good description. Please insert alphabet characters only.`);
+                $('#company').focus();
+            };
+        };
+        companyValidation();
+        console.log($company);
+    
+});//end document.ready 
+
+
 //
-//Phone - Make sure phone has at least 10 digits
-//        function nameValidation(){
-//            if ($phone.match($phoneValidator)===true){
-//                alert("This works!");
-//            } else {
-//               alert("Please make sure to include Alpha-numeric character only."); 
-//            };
-//        };
-//    $('.submit').attr("disabled", true);
-//    return true;   
-//  }); 
-//});
-//
-// 
-//
-//Submit - Make the submit button's opactity become 100% with jQuery once all requirements are met
 //
 //
-//
+////--------------EXPERIMENTAL Submit Opacity function--------------------//      
+////        $('input').on("keydown", function() {
+////            var inputValues = {
+////                $firstname : $('#firstname').val(),
+////                $lastname : $('#lastname').val(),
+////                $email : $('#email').val(),
+////                $phone : $('#phone').val(),
+////                $company : $('#company').val()
+////            }
+////            console.log('inputValues');
+////            
+////            for(var prop in inputValues) {
+////                console.log('inputValues');
+////                
+////                if(inputValues.prop !==""){
+////                    
+////                    //Submit button changes opacity when all 5 fields have inputs filled out
+////                    $('.submit').css('opactity', '1');
+////                    
+////                }
+////            };     
+////        });
+//            
